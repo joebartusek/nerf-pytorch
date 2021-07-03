0f8cbe547af52cdeef30b095f92734df37be6acc
joebartusek <69549337+joebartusek@users.noreply.github.com>
2021-07-03 11:49:56 -0700

    update data download script

--- download_example_data.sh
+++ download_example_data.sh
@@ -1,6 +1,6 @@
 wget http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/tiny_nerf_data.npz
-mkdir -p data
-cd data
+mkdir -p /tigress/jfb4/nerf/data
+cd /tigress/jfb4/nerf/data
 wget http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/nerf_example_data.zip
 unzip nerf_example_data.zip
-cd ..
+cd /home/jfb4/SeeingWithSound/code/nerf-pytorch
